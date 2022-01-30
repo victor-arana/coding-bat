1a9c9ab10d8a7158ec283606e35e3f835817878d
Victor Jose Arana Rodriguez <victorr@backbase.com>
2022-01-30 11:49:17 -0600

    :01-sleep-in: define test
    Changes committed:
    	new file:   src/test/java/com/no/data/WarmupOneTest.java

---
+++ src/test/java/com/no/data/WarmupOneTest.java
@@ -0,0 +1,16 @@
+package com.no.data;
+
+import org.junit.Test;
+
+public class WarmupOneTest {
+
+    @Test
+    public void sleepOnVacationWeekend(){}
+
+    @Test
+    public void sleepOnNonVacationWeekend(){}
+
+    @Test
+    public void sleepOnVacationWeekday(){}
+
+}
